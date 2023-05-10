40bb613a8972b1163087abf0eb6b40aa8f6b2fc7
Alex Nadreau <anadreau42@gmail.com>
2023-05-10 07:06:27 -0700

    tried generate doc cmd

--- lib/downloader.dart
+++ lib/downloader.dart
@@ -1,2 +1,16 @@
 //TODO: #1 @anadreau create isolate function to download yt videos
-class Downloader {}
+import 'dart:io';
+import 'dart:isolate';
+
+import 'package:creator/creator.dart';
+
+final mediaDownloaderCreator = Creator((ref) async {
+  String downloadUrl = '';
+  String fileSavePath = '';
+  final ytDownloadCmd = 'yt-dlp.exe -P $fileSavePath $downloadUrl';
+
+  final result = await Isolate.run(
+      () => Process.start('pwershell.exe', ['-Command', ytDownloadCmd]));
+
+  ///cmd yt-dlp.exe -P C:\Users\anadr\Videos\download *url*
+});
