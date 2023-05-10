ed84a771000ebc862cf899d7a7d0107571b6d58e
Alex Nadreau <anadreau42@gmail.com>
2023-05-09 22:16:03 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DownloaderApp());
+}
+
+class DownloaderApp extends StatelessWidget {
+  const DownloaderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: Placeholder()));
+  }
+}
