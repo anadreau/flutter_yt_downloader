1e4b4ef8778aa245e55930147075c654383e6768
Alex Nadreau <anadreau42@gmail.com>
2023-05-17 14:15:22 -0700

    Updated to switch expression dart 3

--- lib/utils/progress_indicator.dart
+++ lib/utils/progress_indicator.dart
@@ -13,20 +13,12 @@ final statusCreator = Creator((ref) {
   var status = ref.watch(conversionStatusCreator);
   String statusString;
 
-  switch (status) {
-    case Status.notStarted:
-      statusString = 'Download has not been started yet';
-      break;
-    case Status.inProgress:
-      statusString = 'Download in Progress';
-      break;
-    case Status.done:
-      statusString = 'Download has been Completed';
-      break;
-
-    case Status.error:
-      statusString = 'Error Downloading Video';
-  }
+  statusString = switch (status) {
+    Status.notStarted => 'Download has not been started yet',
+    Status.inProgress => 'Download in Progress',
+    Status.done => 'Download has been Completed',
+    Status.error => 'Error Downloading Video'
+  };
 
   return statusString;
 });
