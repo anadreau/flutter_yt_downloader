3b924fbab9c2d2792abf941bf930f47f71fbaac9
Alex Nadreau <anadreau42@gmail.com>
2023-11-03 14:27:06 -0700

    adding plan for validator.

--- lib/install_validation/install_validator.dart
+++ lib/install_validation/install_validator.dart
@@ -4,3 +4,11 @@
 Windows terminal cmd to find path
 Get-command yt-dlp | Select-Object -ExpandProperty Source
 */
+
+//1. check if yt-dlp is installed
+
+//2. download yt-dlp from site
+
+//3. move yt-dlp to designated folder
+
+//4. add yt-dlp to env variables
